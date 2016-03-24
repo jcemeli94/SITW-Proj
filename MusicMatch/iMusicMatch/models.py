ac2a54e2f280d7d152abb0833f8c3dd25d51800b
Joel Cemeli <joelcemeli94@gmail.com>
2016-03-24 10:36:02 +0100

    Implemented the models, on models.py,.- Remains the unicode return

--- MusicMatch/iMusicMatch/models.py
+++ MusicMatch/iMusicMatch/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+#Group, users, playlists, tracks
+
+from django.contrib.auth.models import User
+
+class Group(models.Group):
+    scID = models.IntegerField() #ID used in SoundCloud API
+    userList = models.ManytoManyField(User)
+
+class User(models.User):
+    scID = models.IntegerField()
+    userPlaylists = models.ManytoManyField(Playlist)
+    userTracks = models.ManytoManyField(Track)
+
+class Playlist(models.Playlist):
+    scID = models.IntegerField()
+    ownUsers = models.ManytoManyField(User)
+    trackList = models.ManytoManyField(Track)
+
+class Track(models.Playlist):
+    scID = models.IntegerField()
+    ownUsers = models.ManytoManyField(User)
+    isInPlaylist = models.ManytoManyField(Playlist)
